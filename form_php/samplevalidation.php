74d140d0259863546af5816176f33f245c284b4c
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-02-24 14:14:20 -0600

    Add files via upload

---
+++ form_php/samplevalidation.php
@@ -0,0 +1,61 @@
+<?php
+
+$servername = 'localhost';
+$username = 'saikiran';
+$password = "vS8IxGj4VCi776FT";
+$dbname = "saikiran";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if(array_key_exists('action',$_POST)){
+    $action = $_POST['action'];
+}else{
+    if(array_key_exists('action',$_GET)){
+        $action = $_GET['action'];
+    }
+}
+
+$response = [];
+
+switch($action){
+    case  'addUser':
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $gender = $_POST['gender'];
+        $sql = "INSERT INTO `registration` (`email`, `first`, `last`,`gender`) VALUES ('{$email}', '{$fname}', '{$lname}', '{$gender}')";
+        $result = $conn->query($sql);
+        if(!$result){
+            $response['error'] = "User not added.";
+            $response['success'] = false;
+        }else{
+            $response['success'] = true;
+            $response['data'] = ['email'=>$email];
+        }
+        break;
+    case 'getUsers':
+        $sql = "SELECT * FROM registration";
+        $result = $conn->query($sql);
+        if($result){
+            $response['success'] = true;
+            $response['data'] = [];
+
+            while($row = $result->fetch_assoc()) {
+                $response['data'][] = $row;
+            }
+        }else{
+            $response['error'] = "Query failed.";
+            $response['sql'] = "SELECT * FROM registration";
+        }
+            
+    default:
+        echo"Not a valid action!";
+}
+$conn->close();
+header("Content-Type: application/json");
+echo json_encode($response);
