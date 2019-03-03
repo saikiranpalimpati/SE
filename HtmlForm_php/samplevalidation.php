b5df94b17240bac16a1bfbc709e8bb3392a84af9
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-03-02 19:47:23 -0600

    Update samplevalidation.php

--- HtmlForm_php/samplevalidation.php
+++ HtmlForm_php/samplevalidation.php
@@ -1,22 +1,80 @@
 <?php
-$firstname = $_POST['fname'];
-$lastname = $_POST['lname'];
-$email=$_POST['email'];
-$gender=$_POST['gender'];
-valid==0;
-if(valid ==1){
-$fp = fopen('test.txt','w');
-fwrite($fp,$firstname);
-fwrite($fp,"\t");
-fwrite($fp,$lastname);
-fwrite($fp,"\t");
-fwrite($fp,$email);
-fwrite($fp,"\t");
-fwrite($fp,$gender);
-fwrite($fp,"\t");
-fwrite($fp,"\n");
-fclose($fp);
+
+$servername = 'localhost';
+$username = 'saikiran';
+$password = "vS8IxGj4VCi776FT";
+$dbname = "saikiran";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if(array_key_exists('action',$_POST)){
+    $action = $_POST['action'];
+    console.log("in switch");
+}else{
+    if(array_key_exists('action',$_GET)){
+        $action = $_GET['action'];
+    }
 }
-else{
-"<a history.go(-1)></a>"
+
+$response = [];
+
+switch($action){
+    
+    case  'addUser':
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $gender = $_POST['gender'];
+        $sql = "INSERT INTO `registration` (`email`, `first`, `last`,`gender`) VALUES ('{$email}', '{$fname}', '{$lname}', '{$gender}')";
+        $result = $conn->query($sql);
+        if(!$result){
+            $response['error'] = "User not added.";
+            $response['success'] = false;
+        }else{
+            $response['success'] = true;
+            $response['data'] = ['email'=>$email];
+        }
+        break;
+    case  'updateUser':
+        $fname = $_POST['fname'];
+        
+        $email = $_POST['email'];
+        
+        $sql = "UPDATE `registration` SET `email` = '$email' WHERE `first`= '$fname' ";
+        $result = $conn->query($sql);
+        if(!$result){
+            $response['error'] = "User not added.";
+            $response['success'] = false;
+        }else{
+            $response['success'] = true;
+            $response['data'] = ['email'=>$email];
+        }
+        break;
+
+    case 'getUsers':
+        $sql = "SELECT * FROM registration";
+        $result = $conn->query($sql);
+        if($result){
+            $response['success'] = true;
+            $response['data'] = [];
+
+            while($row = $result->fetch_assoc()) {
+                $response['data'][] = $row;
+            }
+        }else{
+            $response['error'] = "Query failed.";
+            $response['sql'] = "SELECT * FROM registration";
+        }
+            
+    default:
+        
+        echo"Not a valid action!";
 }
+$conn->close();
+header("Content-Type: application/json");
+echo json_encode($response);
