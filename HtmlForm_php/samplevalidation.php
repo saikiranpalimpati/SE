1151a86ecabb1e867935383a3b7b587a09c7c57b
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-04-09 18:32:22 -0500

    Update samplevalidation.php

--- HtmlForm_php/samplevalidation.php
+++ HtmlForm_php/samplevalidation.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 
 if(array_key_exists('action',$_POST)){
     $action = $_POST['action'];
-    console.log("in switch");
+    
 }else{
     if(array_key_exists('action',$_GET)){
         $action = $_GET['action'];
@@ -24,7 +24,6 @@ if(array_key_exists('action',$_POST)){
 $response = [];
 
 switch($action){
-    
     case  'addUser':
         $fname = $_POST['fname'];
         $lname = $_POST['lname'];
@@ -32,6 +31,17 @@ switch($action){
         $gender = $_POST['gender'];
         $sql = "INSERT INTO `registration` (`email`, `first`, `last`,`gender`) VALUES ('{$email}', '{$fname}', '{$lname}', '{$gender}')";
         $result = $conn->query($sql);
+        $myfile = fopen("test.txt", "a") or die("Unable to open file!");
+        fwrite($myfile,$fname);
+        fwrite($myfile,"\t");
+        fwrite($myfile,$lname);
+        fwrite($myfile,"\t");
+        fwrite($myfile,$email);
+        fwrite($myfile,"\t");
+        fwrite($myfile,$gender);
+        fwrite($myfile,"\t");
+        fwrite($myfile,"\n");
+        fclose($myfile);
         if(!$result){
             $response['error'] = "User not added.";
             $response['success'] = false;
@@ -57,24 +67,76 @@ switch($action){
         break;
 
     case 'getUsers':
-        $sql = "SELECT * FROM registration";
+        $sql = "SELECT * FROM `registration`";
         $result = $conn->query($sql);
         if($result){
+            
+            $response['success'] = true;
+            $response['data'] = [];
+            $outstring="";
+            while($row = $result->fetch_assoc()) {
+                $response['data'][] = $row;
+            }
+            
+        }else{
+            $response['error'] = "Query failed.";
+            $response['sql'] = "SELECT * FROM registration";
+        }
+        
+        break;
+    
+    case 'femaleemail':
+        
+        $sql = "SELECT `email` FROM `registration` WHERE `GENDER` = 'female'";
+        $result = $conn->query($sql);
+
+        if($result){
+            $myfile = fopen("femaleEmail.txt", "w") or die("Unable to open file!");
             $response['success'] = true;
             $response['data'] = [];
 
             while($row = $result->fetch_assoc()) {
                 $response['data'][] = $row;
+                fwrite($myfile,"$row[email]");
+                fwrite($myfile,"\n");
             }
+            fclose($myfile);
+        }else{
+            $response['error'] = "Query failed.";
+            $response['sql'] = "SELECT `email` FROM `registration` WHERE `GENDER` = 'female'";
+        }
+        
+        break;
+
+    case 'getEmails':
+        $sql = "SELECT `email` FROM `registration`";
+        $result = $conn->query($sql);
+        if($result){
+            $myfile = fopen("AllEmails.txt", "w") or die("Unable to open file!");
+            fwrite($myfile,"eMAIL");
+            fwrite($myfile,"\n");
+            $response['success'] = true;
+            $response['data'] = [];
+
+            while($row = $result->fetch_assoc()) {
+                $response['data'][] = $row;
+                fwrite($myfile,"$row[email]");
+                fwrite($myfile,"\n");
+            }
+            
+
         }else{
             $response['error'] = "Query failed.";
             $response['sql'] = "SELECT * FROM registration";
         }
-            
+        
+        break;
     default:
         
         echo"Not a valid action!";
 }
+
 $conn->close();
 header("Content-Type: application/json");
 echo json_encode($response);
+//echo json_encode($response['success']);
